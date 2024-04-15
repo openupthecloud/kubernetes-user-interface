7fcb83b21c589b7de6c28f4412ca05f167aba0b5
Louis Bichard <louis@gitpod.io>
2024-04-15 07:14:17 +0000

    fix: initial commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome to the Kubernetes dashboard
+        </p>
+        <div>
+          <p>Hello</p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
