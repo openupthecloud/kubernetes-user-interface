1485832425e54f3c7c97d9067bc769f4c7456c23
Louis Bichard <louis@gitpod.io>
2024-04-16 08:05:33 +0000

    fix: update backend

--- backend/node.js
+++ backend/node.js
@@ -1,17 +1,48 @@
-const k8s = require('@kubernetes/client-node');
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3002;
 
+app.use((_, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+const k8s = require('@kubernetes/client-node');
 const kc = new k8s.KubeConfig();
 kc.loadFromFile('./config.yml');
-
 const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
 
-const main = async () => {
+app.get('/pods', async (_, response) => {
     try {
-        const readNamespaceRes = await k8sApi.readNamespace('default');
+        const readNamespaceRes = await k8sApi.listNamespacedPod("default");
         console.log('Namespace: ', readNamespaceRes.body);
+        const result = readNamespaceRes.body.items.map(({ metadata }) => ({
+            name: metadata.name,
+            labels: metadata.labels,
+        }))
+        response.send(result);
     } catch (err) {
         console.error('err', err);
+        response.send("failure");
     }
-};
+});
 
-main();
+app.get('/namespaces', async (_, response) => {
+    try {
+        const readNamespaceRes = await k8sApi.listNamespace("default");
+        console.log('Namespace: ', readNamespaceRes.body);
+        const result = readNamespaceRes.body.items.map(({ metadata }) => ({
+            name: metadata.name,
+            labels: metadata.labels,
+        }))
+        response.send(result);
+    } catch (err) {
+        console.error('err', err);
+        response.send("failure");
+    }
+});
+
+app.listen(port, function () {
+    console.log("Listening on " + port);
+});
--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,16 +1,62 @@
+import axios from 'axios';
 import './App.css';
+import 'axios';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [pods, setPods] = useState([]);
+  const [namespaces, setNamespaces] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3002/pods")
+      .then((data) => {
+        setPods(data.data)
+      })
+  }, [])
+
+  useEffect(() => {
+    axios.get("http://localhost:3002/namespaces")
+      .then((data) => {
+        setNamespaces(data.data)
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Welcome to the Kubernetes dashboard
-        </p>
-        <div>
-          <p>Hello</p>
-        </div>
-      </header>
+    <div className="App container mx-auto px-24 py-24 h-full bg-gray-600">
+      <h1 className="text-3xl text-white font-bold uppercase">Kubernetes Dashboard</h1>
+      <br />
+      <hr />
+      <h2 className="text-2xl text-white font-bold uppercase">Pods</h2>
+      <table className="text-white table-fixed">
+        <thead>
+          <th>name</th>
+          <th>labels</th>
+        </thead>
+        {
+          pods.map((pod: any) => {
+            return <tr>
+              <td>{pod.name}</td>
+              <td>{pod.labels.toString()}</td>
+            </tr>
+          })
+        }
+      </table>
+      <h2 className="text-2xl text-white font-bold uppercase">Namespaces</h2>
+      <table className="text-white table-fixed">
+        <thead>
+          <th>name</th>
+          <th>labels</th>
+        </thead>
+        {
+          namespaces.map((pod: any) => {
+            return <tr>
+              <td>{pod.name}</td>
+              <td>{pod.labels.toString()}</td>
+            </tr>
+          })
+        }
+      </table>
     </div>
   );
 }
